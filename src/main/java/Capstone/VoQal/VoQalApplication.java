29cbc8898184ff5a9add19169a4fb873423aac7f
cym1212 <william5680@naver.com>
2024-03-28 22:06:05 +0900

    Initial settings

---
+++ src/main/java/Capstone/VoQal/VoQalApplication.java
@@ -0,0 +1,13 @@
+package Capstone.VoQal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoQalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoQalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/VoQal/VoQalApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.VoQal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoQalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
